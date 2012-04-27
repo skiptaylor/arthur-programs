2fd4d598b642e5ea8a056d90ec0569c25bab20bb
Jarrod Taylor <jarrodtaylor@me.com>
2012-04-27 10:49:33 -0400

    Adds auth library

---
+++ libraries/authorization.rb
@@ -0,0 +1,27 @@
+helpers do
+
+	def sign_in user_id
+		user = User.get user_id
+		session[:user] = user.id
+		session[:admin] = user.admin?
+		redirect '/profile'
+	end
+
+	def authorize!
+		unless session[:user]
+			session[:alert] = 'You must sign in to see that page.'
+			session[:alert] = { heading: 'Unauthorized Access', message: 'You must sign in to see that page.' }
+			redirect '/sign-in'
+		end
+	end
+
+	def admin!
+		authorize!
+		user = User.get session[:user]
+		unless user.admin?
+			session[:alert] = { heading: 'Unauthorized Access', message: 'You must be an admin to see that page.' }
+			redirect '/'
+		end
+	end
+
+end
