0e809e16a52ed9c30763a5968b7d25df5a892076
Jarrod Taylor <jarrodtaylor@me.com>
2012-05-31 10:25:31 -0400

    Adds new user creation form in admin section

--- routes/admin/users.rb
+++ routes/admin/users.rb
@@ -63,6 +63,56 @@ post '/admin/users/:id/?' do
 	redirect "/admin/users/#{user.id}"
 end
 
+get '/admin/user/new/?' do
+	admin!
+	@user = User.new
+	view 'admin/user'
+end
+
+post '/admin/user/new/?' do
+	admin!
+	
+	params[:email].strip!
+	params[:email].downcase!
+	
+	params[:name].strip!
+
+	params[:phone].strip!
+	params[:phone].downcase!
+	
+	params[:password].strip!
+	params[:password].downcase!
+	
+	params[:max_exams].is_numeric? ? params[:max_exams] = params[:max_exams].to_i : params[:max_exams] = user.max_exams
+	params[:max_scenarios].is_numeric? ? params[:max_scenarios] = params[:max_scenarios].to_i : params[:max_scenarios] = user.max_scenarios
+
+	user = User.create(
+		admin: 					 	false,
+		email: 					 	params[:email],
+		password: 			 	params[:password],
+		name:  				 	 	params[:name],
+		phone: 				 	 	params[:phone],
+		notes:					 	params[:notes],
+		max_exams: 		 	 	params[:max_exams],
+		max_scenarios: 	 	params[:max_scenarios],
+		ncmhce_downloads: false,
+		nce_downloads: 		false,
+		expiration_date: Date.from_fields(
+			params[:expiration_year],
+			params[:expiration_month],
+			params[:expiration_day]
+		)
+	)
+	
+	user.update(admin: true) if params[:admin]
+	
+	user.update(ncmhce_downloads: true) if params[:ncmhce_downloads]
+	user.update(nce_downloads: true) if params[:nce_downloads]
+
+	session[:alert] = { style: 'alert-success', message: "#{user.name} has been created." }
+	redirect "/admin/users/#{user.id}"
+end
+
 get '/admin/users/:id/delete/?' do
 	admin!
 	@user = User.get params[:id]
