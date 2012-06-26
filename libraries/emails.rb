22f105700b99b6f7ecadc750ea77f489ee71200f
Jarrod Taylor <jarrodtaylor@me.com>
2012-06-26 01:40:57 -0400

    Fixes MS Word bugs

--- libraries/emails.rb
+++ libraries/emails.rb
@@ -4,7 +4,7 @@ class Email
 	
 		body = ''
 		body << "<p>Dear #{name},</p>"
-		body << "<p>Welcome - you have successfully created an account at counselorexams.com!  You will receive a separate email confirming the details of your purchase.</p>"
+		body << "<p>Welcome - you have successfully created an account at counselorexams.com! You will receive a separate email confirming the details of your purchase.</p>"
 		body << "<p>Thank you for choosing us as a source for your exam preparatory materials. We are confident our exam prep services can help you pass your counselor licensure exam, because they have helped thousands of previous customers do just that.</p>"
 		body << "<h3>Access Instructions:</h3>"
 		body << "<p>To access your account profile, which lists the products you have purchased and provides access to your online services, go to: http://counselorexams/sign-in</p>"
@@ -20,7 +20,7 @@ class Email
 			body << "<p>The customer has 3 days from the purchase date to review the purchased <em>online services,</em> including online Scenarios and eBook, to confirm that it is what he or she expected. If the customer has purchased a hard-copy of the NCMHCE Supplement, he or she has <em>3 days from the date of delivery of the Supplement</em>. If unhappy with the content for any reason, the customer is required to notify CTC within these 3 days either by phone or e-mail that he/she is requesting a refund and will be returning the Supplement, if applicable. Further, to qualify for a refund, the customer <em>may not have accessed more than 3 online scenarios</em>. Upon proper notification, CTC will turn off access to these online services, and a refund will be issued. For those customers who have purchased the hard-copy, it is required the hard-copy Supplement be returned to CTC in good condition. Once CTC has received the hard-copy, a refund will be issued (excludes all shipping and any applicable sales tax). Note: The return expense is the responsibility of the customer.</p>"
 		end
 	
-		body << "<p>If you have any questions about your purchase, please contact us at http://counselorexams.com/feedback.</p>"
+		body << "<p>If you have any questions about your purchase, please contact us at http://counselorexams.com/feedback.</p>"
 		body << "<p>We are also available by phone during the business hours of 9:30 AM to 4:30 PM Eastern time, Monday through Friday, at: 888-326-9229.</p>"
 	
 		Pony.mail(
@@ -36,12 +36,12 @@ class Email
 	
 		body = ''
 		body << "<p>Dear #{name},</p>"
-		body << "<p>Thank you for your purchase at counselorexams.com!</p>"
+		body << "<p>Thank you for your purchase at counselorexams.com!</p>"
 		body << "<p>We are confident our preparatory materials will help you pass your counselor licensure exam, because they have helped thousands of previous customers do just that.</p>"
 		body << "<p>The online products you have purchased should be immediately available at your profile page. If you purchased a hard-copy of any study materials, you will receive a separate email from FedEx with a tracking number once it is shipped. (Shipping usually occurs within 2 business days.)</p>"
 		body << "<h3>Your Purchase</h3>"
 		body << "<p>#{item}: $#{price}</p>"
-		body << "<p>If you have any questions about your purchase, please contact us at http://counselorexams.com/feedback.</p>"
+		body << "<p>If you have any questions about your purchase, please contact us at http://counselorexams.com/feedback.</p>"
 		body << "<p>We are also available by phone during the business hours of 9:30 AM to 4:30 PM Eastern time, Monday through Friday, at: 888-326-9229.</p>"
 	
 		Pony.mail(
