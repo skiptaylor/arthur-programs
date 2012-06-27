9fafd61345ea16884b099900309f51ba10b1d5e7
Skip Taylor <tayloraid@gmail.com>
2012-06-27 12:42:53 -0400

    Corrects email content again

--- libraries/emails.rb
+++ libraries/emails.rb
@@ -5,7 +5,7 @@ class Email
 		body = ''
 		body << "<p>Dear #{name},</p>"
 		body << "<p>Welcome - you have successfully created an account at counselorexams.com! You will receive a separate email confirming the details of your purchase.</p>"
-		body << "<p>Thank you for choosing us as a source for your exam preparatory materials. We are confident our exam prep services can help you pass your counselor licensure exam, because they have helped thousands of previous customers do just that.</p>"
+		body << "<p>Thank you for choosing us as a source for your exam preparatory materials. We are confident our prep services can help you pass your counselor licensure exam, because they have helped thousands of previous customers do just that.</p>"
 		body << "<h3>Access Instructions:</h3>"
 		body << "<p>To access your account profile, which lists the products you have purchased and provides access to your online services, go to: http://counselorexams/sign-in</p>"
 		body << "<p>Your account email address is: #{email}</p>"
@@ -47,7 +47,7 @@ class Email
 			headers: { 'Content-Type' => 'text/html' },
 			to: to,
 			from: 'no-reply@counselorexams.com',
-			subject: 'Welcome to counselorexams.com',
+			subject: 'counselorexams.com purchase confirmation',
 			body: body
 		)
 	end
