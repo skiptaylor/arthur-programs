669374369b9d01bfb882d4d4fa524be4ef70f582
Jarrod Taylor <jarrodtaylor@me.com>
2013-07-28 15:55:38 -0400

    Adds messages

---
+++ routes/admin/messages.rb
@@ -0,0 +1,27 @@
+get '/admin/messages/?' do
+  admin!
+  
+  @messages = Message.all
+  
+  erb :'admin/messages'
+end
+
+post '/admin/messages/?' do
+  admin!
+  
+  message = Message.create(body: params[:body].strip)
+  message.update(ncmhce: true) if params[:ncmhce]
+  message.update(nce: true) if params[:nce]
+  message.update(profile: true) if params[:profile]
+  
+  redirect '/admin/messages'
+end
+
+get '/admin/messages/:id/delete/?' do
+  admin!
+  
+  message = Message.get(params[:id])
+  message.destroy
+  
+  redirect '/admin/messages'
+end
