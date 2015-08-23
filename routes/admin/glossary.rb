b982ce63ba9f305e441f02e0010984f1a4b3d54d
Skip Taylor <tayloraid@gmail.com>
2015-08-23 14:17:10 -0400

    Changes term to id

--- routes/admin/glossary.rb
+++ routes/admin/glossary.rb
@@ -45,33 +45,67 @@ post '/admin/glossary/new/?' do
 	redirect "/admin/glossary/#{params[:exam].downcase}"
 end
 
+
+
 get '/admin/glossary/:id/?' do
-	admin!
-	@term = Glossary.first term: params[:id]
-	erb :'admin/term'
+  admin!
+  @term = Glossary.get params[:id]
+  erb :'admin/term'
 end
 
 post '/admin/glossary/:id/?' do
-	admin!
-	@term = Glossary.first term: params[:id]
-	@term.update(
-		term: params[:term],
-		definition: params[:definition],
+  admin!
+  @term = Glossary.get params[:id]
+  @term.update(
+    term: params[:term],
+    definition: params[:definition],
     short_def: params[:short_def],
-		exam: params[:exam],
-		chapter: params[:chapter]
-	)
-	session[:alert] = {
-		style: 'alert-success',
-		message: "#{params[:term]} has been updated."
-	}
-	params[:exam] = 'other' if params[:exam].empty?
-	redirect "/admin/glossary/#{params[:exam].downcase}"
+    exam: params[:exam],
+    chapter: params[:chapter]
+  )
+  session[:alert] = {
+    style: 'alert-success',
+    message: "#{params[:term]} has been updated."
+  }
+  params[:exam] = 'other' if params[:exam].empty?
+  redirect "/admin/glossary/#{params[:exam].downcase}"
 end
+
+
+
+
+
+# get '/admin/glossary/:id/?' do
+#   admin!
+#   @term = Glossary.first term: params[:id]
+#   erb :'admin/term'
+# end
+#
+# post '/admin/glossary/:id/?' do
+#   admin!
+#   @term = Glossary.first term: params[:id]
+#   @term.update(
+#     term: params[:term],
+#     definition: params[:definition],
+#     short_def: params[:short_def],
+#     exam: params[:exam],
+#     chapter: params[:chapter]
+#   )
+#   session[:alert] = {
+#     style: 'alert-success',
+#     message: "#{params[:term]} has been updated."
+#   }
+#   params[:exam] = 'other' if params[:exam].empty?
+#   redirect "/admin/glossary/#{params[:exam].downcase}"
+# end
+
+
+
+
 
 get '/admin/glossary/:id/delete/?' do
 	admin!
-	term = Glossary.first term: params[:id]
+	term = Glossary.get params[:id]
 	term.destroy
 		session[:alert] = {
 		style: 'alert-success',
