e47476f6ab62126dcb597921e50c961561221ca6
Skip Taylor <tayloraid@gmail.com>
2015-10-01 11:54:59 -0400

    new welcome page

--- routes/ncmhce.rb
+++ routes/ncmhce.rb
@@ -101,8 +101,7 @@ get '/ncmhce/scenarios/:id/score/?' do
 	@breakdown = {}
 	@breakdown['Information Gathering'] = {possible: 0, correct: 0}
 	@breakdown['Decision Making']				= {possible: 0, correct: 0}
-  
-  
+    
 	@answers.each do |a|
 		@breakdown[a.question.score_type][:possible] += a.value if a.value > 0
 	end
@@ -111,6 +110,8 @@ get '/ncmhce/scenarios/:id/score/?' do
 		@breakdown[s.score_type][:correct]  += s.value
 	end
 
+
+  
 	erb :'ncmhce/scenario'
 end
 
