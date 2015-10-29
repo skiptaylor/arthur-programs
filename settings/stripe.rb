4a8e15239d6bb31d9ae287cb02f670d5c35161b9
Skip Taylor <tayloraid@gmail.com>
2015-10-29 13:19:49 -0400

    sets stripe pub key

--- settings/stripe.rb
+++ settings/stripe.rb
@@ -3,7 +3,7 @@ configure :development do
 	STRIPE_PUBLISH_KEY = 'pk_test_bhOUFL32WEOrMD5V8tk4NFq7'
 end
 
-# configure :production do
-#   STRIPE_PRIVATE_KEY = ''
-#   STRIPE_PUBLISH_KEY = ''
-# end
+configure :production do
+  STRIPE_PRIVATE_KEY = 'sk_test_ps6ty0Tou7rd8lRyUhzMfHsy'
+  STRIPE_PUBLISH_KEY = 'pk_test_bhOUFL32WEOrMD5V8tk4NFq7'
+end
