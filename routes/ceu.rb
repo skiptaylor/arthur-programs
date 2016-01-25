8a1bff3624a7589c529b8ca356e94105804bba8c
Skip Taylor <tayloraid@gmail.com>
2016-01-25 17:33:26 -0500

    Adds 13.95 hard copy and CEU certificate PDF

--- routes/ceu.rb
+++ routes/ceu.rb
@@ -25,7 +25,6 @@ get '/ceu/scenarios/?' do
 end
 
 get '/ceu/scenarios/:id/?' do
-	
 
 	@scenario = Scenario.get params[:id]
 
@@ -55,7 +54,6 @@ get '/ceu/scenarios/:id/?' do
 end
 
 get '/ceu/scenarios/:id/score/?' do
-	
 
 	@scenario = Scenario.get params[:id]
 
@@ -115,14 +113,10 @@ get '/ceu/scenarios/:id/score/?' do
     possible: @breakdown["Decision Making"][:possible],
     correct: @breakdown["Decision Making"][:correct]
   )
+    
+	erb :'ceu/scenario'  
   
-	erb :'ceu/scenario'
 end
-
-
-
-
-
 
 post '/ceu/scenarios/:id/score/?' do
 
@@ -137,20 +131,122 @@ post '/ceu/scenarios/:id/score/?' do
   session[:alert] = { style: 'alert-success',
        message: "Your CEU Certificate has been submitted." }
   redirect "/ceu/scenarios/#{params[:id]}/score"
-end
-
-
-
-
-
-
-get '/ceu/ceu_certificate/?' do
-	authorize!
-	@user = User.get session[:user]
-	
   
-  erb :'ceu/ceu_certificate'
 end
+
+
+
+
+
+
+
+get '/ceu/scenarios/:id/ceu-certificate/?' do
+  
+	@scenario = Scenario.get params[:id]
+  
+  @user = User.get session[:user]
+    
+  @uses = Use.all(user_id: session[:user], scenario_id: params[:id])
+    
+  content_type 'application/pdf'
+  
+  
+  
+  pdf = Prawn::Document.new(:page_size => "LETTER", :page_layout => :landscape)
+  
+
+  pdf.stroke do
+    pdf.rounded_rectangle [0, 540], 720, 540, 16
+    pdf.line_width = 12
+    pdf.stroke_color = '535353'
+  end
+
+
+
+    pdf.image "img/ctc.png", :width => 140, :at => [36, 516]
+
+    pdf.image "img/nbcc.png", :width => 220, :at => [440, 510]
+
+    pdf.move_down(84)
+
+    pdf.move_down(30)
+    pdf.default_leading 2
+    pdf.font "Helvetica"
+
+    pdf.font_size 40
+    pdf.text "Certificate of Completion", :color => "0e5fe1", :align => :center
+
+    pdf.move_down(8)
+    pdf.font_size 26
+    pdf.text "<b>#{@user.name}</b>", :inline_format => true, :align => :center
+
+
+    pdf.move_down(8)
+    pdf.font_size 14
+
+    @uses.each do |u|
+
+      pdf.text "#{u.cert_date.display_short}", :inline_format => true, :align => :center
+
+    
+
+
+
+
+    pdf.move_down(10)
+    pdf.font_size 13
+    pdf.text "License/Credential Number:<b> #{@user.license}</b>", :inline_format => true, :align => :center
+    pdf.move_down(20)
+    pdf.text "On #{u.cert_date.display_short} #{@user.name} successfully completed all requirements <br />of the Arthur-Brende CEU Study Program for CEU Scenario #{@scenario.id}.", :inline_format => true, :align => :center
+
+# <p class="center"><strong>On <%= u.cert_date.display_short %> <%= @user.name %> successfully completed all requirements <br />of the Arthur-Brende CEU Study Program for CEU Scenario <%= @scenario.id %>.</strong></p>
+pdf.move_down(10)
+pdf.text "<#{@user.name} is awarded 2 hours of Continuing Education Units (CEU) for CEU Scenario #{@scenario.id}", :inline_format => true, :align => :center
+end
+# <div class="row">
+#   <div class="span3" style="margin-left: 6em;">
+#     <span style="font-family: fantasy; font-size: 2em; color: #1c89de;">Gary Arthur</span><br />
+#     <small>Gary L. Arthur, Ed., LPC, NCC<br />
+#     License #: xxxxxxxxxx</small>
+#   </div>
+#   <div class="span3 pull-right">
+#     <span style="font-family: fantasy; font-size: 2em; color: #1c89de;">Joel Brende</span><br />
+#     <small>Joel O. Brende, M.D.<br />
+#     License #: xxxxxxxxxx</small>
+#   </div>
+# </div>
+  pdf.move_down(90)
+  pdf.line_width 0.3
+  pdf.stroke_horizontal_rule
+
+  pdf.move_down(10)
+  pdf.font_size 10
+  pdf.text "NBCC-Approved Continuing Education Provider Number XXXXX", :inline_format => true, :align => :center
+
+  pdf.move_down(5)
+
+  pdf.text "Career Training Concepts, Inc.    3640 Hewatt Court, Snellville, GA 30039    Toll Free: 888-326-9229     Fax: 770-672-8099    www.counselorexams.com", :inline_format => true, :align => :center
+
+
+  pdf.render
+
+
+
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 get '/ceu/scenarios/:id/restart/?' do
 	authorize!
