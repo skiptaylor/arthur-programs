ab2887991dcbd328d6700610d8a4760cc78ce593
Skip Taylor <tayloraid@gmail.com>
2016-04-25 10:39:55 -0400

    Fixes email for second chance update

--- libraries/emails.rb
+++ libraries/emails.rb
@@ -63,7 +63,7 @@ class Email
 		body << "<p>Dear #{name},</p>"
 		body << "<p>Thank you for your purchase at counselorexams.com!</p>"
 		body << "<p>As a reminder, you purchased the NCMHCE Starter Package, which provides access to any 12 of our 36 online practice scenarios.</p>"
-		body << "<p>Tip: as you decide which scenarios to access, please be aware that our scenarios get progressively more complex/difficult as the number of the scenario increases. Therefore, to get the most out of your purchase, you may want to try a range of the scenarios, versus just the "first 12."
+		body << "<p>Tip: as you decide which scenarios to access, please be aware that our scenarios get progressively more complex/difficult as the number of the scenario increases. Therefore, to get the most out of your purchase, you may want to try a range of the scenarios, versus just the 'first 12'.
 </p>"
     body << "<p>Depending on your specific situation and relative readiness for the exam, you may find the Starter Package is sufficient to prepare for the NCMHCE. However, we do have customers who state they wished they had purchased the Full Package shortly after their Starter purchase. For these customers, we are now offering a 'second chance' to save!</p>"
 		body << "<p>Second Chance Offer: Should you decide you want access to all 36 of the scenarios within two weeks of your original purchase date, you may upgrade your Starter Package to our Full Package for the price difference of $110. (This would equal the same price as if you had ordered the Full Package originally.)</p>"
