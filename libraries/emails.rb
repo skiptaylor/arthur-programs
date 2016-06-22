08e42ba0d0420a960083f3b7f7204688b4eaec0a
Skip Taylor <tayloraid@gmail.com>
2016-06-22 10:07:10 -0400

    Tests second chance email trigger

--- libraries/emails.rb
+++ libraries/emails.rb
@@ -11,17 +11,19 @@ class Email
 		body << "<p><strong>NOTE: You are responsible for maintaining the confidentiality of the log-in credentials and are solely responsible for all activities that occur under your account. Your account is ONLY FOR PERSONAL USE. You may NOT authorize others to use the account, and you may NOT assign or otherwise transfer the account to any other person or entity. You agree to safeguard the login information used to access this site. You agree not to disclose or share your login information with any third party. You authorize DGA/CTC to assume that any user accessing the site with your log-in credentials is you, the authorized user.</strong></p>"
     body << "<p>Your account email address is: #{email}</p>"
 		body << "<p>If you do not remember the password you used when you created your account, you may use the 'forgot password' link at the above URL to change it.</p>"
-		body << "<h3>Satisfaction Guarantee Return Policy:</h3>"
+		
 		
 		if policy == 'nce'
+      body << "<h3>Satisfaction Guarantee Return Policy:</h3>"
 			body << "<p>The customer has <em>3 days</em> from the purchase date to review the purchased <em>online services</em>, including online exams and eBook, to confirm that it is what he or she expected. If the customer has purchased a hard-copy of the NCE  Study Guide, he or she has <em>3 days from the date of delivery of the Study Guide</em>. If unhappy with the content for any reason, the customer is required to notify CTC within these 3 days either by phone or e-mail that he/she is requesting a refund and will be returning the Study Guide, if applicable. Further, to qualify for a refund, the customer <em>may not have completed any online exam; they are allowed access for review only</em> if they wish to request a Satisfaction Guarantee refund. Upon proper notification, CTC will turn off access to these online services, and a refund will be issued. For those customers who have purchased the hard-copy, it is required the hard-copy Study Guide be returned to CTC in good condition. Once CTC has received the hard-copy, a refund will be issued (excludes all shipping and any applicable sales tax). Note: The return expense is the responsibility of the customer.</p>"
 		end
     
 		if policy == 'ceu'
-			body << "<p>The customer has 3 days from the purchase date to review the purchased <em>online services,</em> including online Scenarios, to confirm that it is what he or she expected. If unhappy with the content for any reason, the customer is required to notify CTC within these 3 days either by phone or e-mail that he/she is requesting. Further, to qualify for a refund, the customer <em>may not have accessed more than 1 online scenario</em>. Upon proper notification, CTC will turn off access to these online services, and a refund will be issued.</p>"
+			body << ""
 		end
 		
 		if policy == 'ncmhce'
+      body << "<h3>Satisfaction Guarantee Return Policy:</h3>"
 			body << "<p>The customer has 3 days from the purchase date to review the purchased <em>online services,</em> including online Scenarios and eBook, to confirm that it is what he or she expected. If the customer has purchased a hard-copy of the NCMHCE Supplement, he or she has <em>3 days from the date of delivery of the Supplement</em>. If unhappy with the content for any reason, the customer is required to notify CTC within these 3 days either by phone or e-mail that he/she is requesting a refund and will be returning the Supplement, if applicable. Further, to qualify for a refund, the customer <em>may not have accessed more than 3 online scenarios</em>. Upon proper notification, CTC will turn off access to these online services, and a refund will be issued. For those customers who have purchased the hard-copy, it is required the hard-copy Supplement be returned to CTC in good condition. Once CTC has received the hard-copy, a refund will be issued (excludes all shipping and any applicable sales tax). Note: The return expense is the responsibility of the customer.</p>"
 		end
 	
@@ -57,29 +59,37 @@ class Email
 		)
 	end
   
-	def self.secondchance to, name
+  
+  
+  if params[:package] == 'Second Chance Upgrade'
+  
+  	def self.secondchance to, name
 	
-		body = ''
-		body << "<p>Dear #{name},</p>"
-		body << "<p>Thank you for your purchase at counselorexams.com!</p>"
-		body << "<p>As a reminder, you purchased the NCMHCE Starter Package, which provides access to any 12 of our 36 online practice scenarios.</p>"
-		body << "<p>Tip: as you decide which scenarios to access, please be aware that our scenarios get progressively more complex/difficult as the number of the scenario increases. Therefore, to get the most out of your purchase, you may want to try a range of the scenarios, versus just the 'first 12'.
-</p>"
-    body << "<p>Depending on your specific situation and relative readiness for the exam, you may find the Starter Package is sufficient to prepare for the NCMHCE. However, we do have customers who state they wished they had purchased the Full Package shortly after their Starter purchase. For these customers, we are now offering a 'second chance' to save!</p>"
-		body << "<p>Second Chance Offer: Should you decide you want access to all 36 of the scenarios within two weeks of your original purchase date, you may upgrade your Starter Package to our Full Package for the price difference of $110. (This would equal the same price as if you had ordered the Full Package originally.)</p>"
-		body << "<p>Note this is in contrast to the pricing of ordering additional scenarios after the two week period has passed, when the upgrade package(s) cost $80.00 per set of 12 scenarios (so the total would be $240 for access to all of the scenarios once the two weeks is up).
-</p>"
-		body << "<p>To upgrade your package from Starter to Full to save this $50.00, sign-in to your account and select the 'Upgrade to Full Package now for $110' button on your Profile page</p>"
-    body << "<p>Good luck on your exam!</p>"
+  		body = ''
+  		body << "<p>Dear #{name},</p>"
+  		body << "<p>Thank you for your purchase at counselorexams.com!</p>"
+  		body << "<p>As a reminder, you purchased the NCMHCE Starter Package, which provides access to any 12 of our 36 online practice scenarios.</p>"
+  		body << "<p>Tip: as you decide which scenarios to access, please be aware that our scenarios get progressively more complex/difficult as the number of the scenario increases. Therefore, to get the most out of your purchase, you may want to try a range of the scenarios, versus just the 'first 12'.
+  </p>"
+      body << "<p>Depending on your specific situation and relative readiness for the exam, you may find the Starter Package is sufficient to prepare for the NCMHCE. However, we do have customers who state they wished they had purchased the Full Package shortly after their Starter purchase. For these customers, we are now offering a 'second chance' to save!</p>"
+  		body << "<p>Second Chance Offer: Should you decide you want access to all 36 of the scenarios within two weeks of your original purchase date, you may upgrade your Starter Package to our Full Package for the price difference of $110. (This would equal the same price as if you had ordered the Full Package originally.)</p>"
+  		body << "<p>Note this is in contrast to the pricing of ordering additional scenarios after the two week period has passed, when the upgrade package(s) cost $80.00 per set of 12 scenarios (so the total would be $240 for access to all of the scenarios once the two weeks is up).
+  </p>"
+  		body << "<p>To upgrade your package from Starter to Full to save this $50.00, sign-in to your account and select the 'Upgrade to Full Package now for $110' button on your Profile page</p>"
+      body << "<p>Good luck on your exam!</p>"
 	
-		Pony.mail(
-			headers: { 'Content-Type' => 'text/html' },
-			to: to,
-			from: 'no-reply@counselorexams.com',
-			subject: 'counselorexams.com NCMHCE Second Chance Upgrade',
-			body: body
-		)
-	end
+  		Pony.mail(
+  			headers: { 'Content-Type' => 'text/html' },
+  			to: to,
+  			from: 'no-reply@counselorexams.com',
+  			subject: 'counselorexams.com NCMHCE Second Chance Upgrade',
+  			body: body
+  		)
+  	end
+  
+  end
+  
+  
   
 
 end
