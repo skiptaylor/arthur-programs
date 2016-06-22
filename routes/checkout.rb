0bc908c3f9371ae562ccebab34c2b087b0e8e202
Skip Taylor <tayloraid@gmail.com>
2016-06-22 11:57:43 -0400

    Uses if else for emails

--- routes/checkout.rb
+++ routes/checkout.rb
@@ -179,10 +179,13 @@ post '/checkout/:product/?' do
   
     
     if settings.environment == 'production'
-      Email.welcome(user.email, user.name, user.email, email)
-      Email.receipt(user.email, user.name, "#{params[:package]} #{params[:optional]}", params[:amount])
       if params[:package] = 'NCMHCE: Starter Package'
+        Email.welcome(user.email, user.name, user.email, email)
+        Email.receipt(user.email, user.name, "#{params[:package]} #{params[:optional]}", params[:amount])
         Email.secondchance(user.email, user.name)
+      else  
+        Email.welcome(user.email, user.name, user.email, email)
+        Email.receipt(user.email, user.name, "#{params[:package]} #{params[:optional]}", params[:amount])
       end
     end
     
