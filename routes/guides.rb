e8df5052184aa359190aec5b69896687ee6eb822
Skip Taylor <tayloraid@gmail.com>
2016-08-11 12:54:06 -0500

    Changes chapters nav and styling

--- routes/guides.rb
+++ routes/guides.rb
@@ -1,16 +1,16 @@
 get '/manuals/:manual_id/manuals/?' do
-	authorize!
-	@user = User.get session[:user]
-  @manual = Manual.get(params[:manual_id])
+   authorize!
+   @user = User.get session[:user]
+   @manual = Manual.get(params[:manual_id])
 	
 	erb :'manuals/ncmhce_guide'
 end
 
 get '/manuals/:manual_id/sections/?' do
-	authorize!
-	@user = User.get session[:user]
-  @manual = Manual.get(params[:manual_id])
-  @section = @manual.sections
+   authorize!
+   @user = User.get session[:user]
+   @manual = Manual.get(params[:manual_id])
+   @section = @manual.sections
 	
 	erb :'manuals/sections'
 end
@@ -25,3 +25,12 @@ get '/manuals/:manual_id/sections/:section_id/chapters/?' do
   erb :'manuals/chapters'
 end
 
+get '/manuals/:manual_id/sections/:section_id/chapters/:chapter_id/chapter/?' do
+  authorize!
+  @user = User.get session[:user]
+  @manual = Manual.get(params[:manual_id])
+  @section = @manual.sections
+  @chapter = Chapter.get(params[:chapter_id])
+
+  erb :'manuals/chapters'
+end
