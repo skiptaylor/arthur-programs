bc1d4f80a70ef22a8bcad1947c9a50f3a69cf875
Skip Taylor <tayloraid@gmail.com>
2018-12-05 11:01:11 -0500

    Adds callback

--- routes/feedback.rb
+++ routes/feedback.rb
@@ -1,7 +1,7 @@
 post '/feedback/?' do
 	Pony.mail(
 		headers: { 'Content-Type' => 'text/html' },
-		to: 'counselorexams@careertrain.com, skip@tayloraid.com',
+		to: 'skiptaylor101@gmail.com.com',
 		from: 'feedback@counselorexams.com',
 		subject: params[:subject],
 		body: "#{markdown params[:msg]}<hr />#{params[:name]}<br />#{params[:email]}"
